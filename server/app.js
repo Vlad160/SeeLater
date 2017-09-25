238cf7c78b875574ce082a3679e3689712b92b6c
Vlad160 <vlaner160@gmail.com>
2017-09-25 23:59:19 +0300

    Removed useless staff

--- server/app.js
+++ server/app.js
@@ -3,7 +3,6 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.set('port', (process.env.PORT || 3000));
-app.use(express.static('app', { redirect: false }));
 app.use(bodyParser.json());
 
 app.listen(app.get('port'), () => console.log('Site started on port ', app.get('port')));
